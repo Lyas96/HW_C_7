2df5610e5f232eef1ee41e181b979c0b0c69cbf4
Lyas <Egorlyasotsky@yandex.ru>
2023-04-20 16:04:01 +0400

    1st task is ready

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -7,3 +7,41 @@ m = 3, n = 4.
 1 -3,3 8 -9,9
 
 8 7,8 -7,1 9 */
+
+using System;
+using static System.Console;
+
+Write("Введите число сторок: ");
+int raws = int.Parse(ReadLine());
+Write("Введите число столбцов: ");
+int columns = int.Parse(ReadLine());
+double[,] array = GetNewArray(raws, columns, -10, 10);
+PrintArray(array);
+WriteLine();
+
+
+double[,] GetNewArray(int m, int n, int minValue, int maxValue)
+{
+  double[,] result = new double[m, n];
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      result[i, j] = new Random().Next(minValue, maxValue + 1) + new Random().NextDouble();
+    }
+  }
+  return result;
+
+}
+
+void PrintArray(double[,] inArray)
+{
+  for (int i = 0; i < inArray.GetLength(0); i++)
+  {
+    for (int j = 0; j < inArray.GetLength(1); j++)
+    {
+      Write($"{inArray[i, j]:f1} ");
+    }
+    WriteLine();
+  }
+}
