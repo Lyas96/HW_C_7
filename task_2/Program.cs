ef8577b05b06b5a99702617dae4b5062c4479b12
Lyas <Egorlyasotsky@yandex.ru>
2023-04-20 16:32:53 +0400

    2nd task is ready

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -9,3 +9,57 @@
 8 4 2 4
 
 17 -> такого числа в массиве нет */
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите число сторок: ");
+int raws = int.Parse(ReadLine());
+Write("Введите число столбцов: ");
+int columns = int.Parse(ReadLine());
+int[,] array = GetNewArray(raws, columns, 0, 10);
+PrintArray(array);
+WriteLine();
+Write("Введите позицию необходимого элемента через пробел: ");
+string[] Position = ReadLine().Split(" ");
+int raw = int.Parse(Position[0]);
+int column = int.Parse(Position[1]);
+
+if (raw <= raws && column <= columns)
+{
+WriteLine($"{array[raw, column]}");
+}
+else
+{
+  WriteLine("Число с указанной позицией не существует в заданном массиве");
+}
+
+
+int[,] GetNewArray(int m, int n, int minValue, int maxValue)
+{
+  int[,] result = new int[m, n];
+  for (int i = 0; i < m; i++)
+  {
+    for (int j = 0; j < n; j++)
+    {
+      result[i, j] = new Random().Next(minValue, maxValue + 1);
+    }
+  }
+  return result;
+
+}
+
+void PrintArray(int[,] inArray)
+{
+  for (int i = 0; i < inArray.GetLength(0); i++)
+  {
+    for (int j = 0; j < inArray.GetLength(1); j++)
+    {
+      Write($"{inArray[i, j]} ");
+    }
+    WriteLine();
+  }
+}
+
+
